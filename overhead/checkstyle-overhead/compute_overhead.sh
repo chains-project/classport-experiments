e87f0397b161d42db822193407c3b3032143080d
serenacofano <cofanoserena@gmail.com>
2025-06-05 13:39:13 +0000

    update in checkstyle script

--- overhead/checkstyle-overhead/compute_overhead.sh
+++ overhead/checkstyle-overhead/compute_overhead.sh
@@ -2,5 +2,6 @@
 
 # This script computes the overhead of the classport embedding process for PDFBox.
 # It assumes that the classport files are already present in the specified directory.
-mvn clean package -Dmaven.repo.local=../../checkstyle-checkstyle-10.23.0/classport-files
+mvn clean 
+mvn -P assembly package -Dmaven.repo.local=../../checkstyle-checkstyle-10.23.0/classport-files 
 java -jar target/checkstyle-overhead-1.0-SNAPSHOT.jar -rff results.json -rf json
